c08ba1e678123c0ea0e4195d1c360b5c1361853f
unknown <tungdao923@gmail.com>
2022-10-24 16:20:02 +0700

    up

---
+++ src/main/java/rikkei/academy/repository/IProvinceRepository.java
@@ -0,0 +1,10 @@
+package rikkei.academy.repository;
+
+import rikkei.academy.model.Province;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProvinceRepository extends PagingAndSortingRepository<Province, Long> {
+
+}
---
+++ src/main/java/rikkei/academy/service/customer/CustomerServiceIMPL.java
@@ -0,0 +1,45 @@
+package rikkei.academy.service.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import rikkei.academy.model.Customer;
+import rikkei.academy.model.Province;
+import rikkei.academy.repository.ICustomerRepository;
+
+import java.util.Optional;
+
+public class CustomerServiceIMPL implements ICustomerService{
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    @Override
+    public Iterable<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable) {
+        return customerRepository.findAllByFirstNameContaining(firstname, pageable);
+    }
+}
